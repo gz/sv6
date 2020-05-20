27e79e0f97f16dd6c307fc1680110e0124c4c36f
Sanidhya Kashyap <sanidhya@gatech.edu>
2020-05-20 15:20:34 -0400

    proper name of the file

--- parse.py
+++ parse.py
@@ -5,7 +5,7 @@ import csv
 NUM_SECONDS = 5
 
 if __name__ == '__main__':
-    with open('sv6-mmaps.csv', 'w', newline='') as csvfile:
+    with open('sv6_results.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(['threads', 'throuhput'])
